24172602ea18de36403c4cadf61cee2e1fddbe2e
auddnjs2008 <auddnjs2008@naver.com>
2026-03-24 06:50:57 +0900

    fix card 상세모달 width 넓힘

--- src/app/(afterLogin)/(main)/@modal/(.)cards/[cardId]/page.tsx
+++ src/app/(afterLogin)/(main)/@modal/(.)cards/[cardId]/page.tsx
@@ -19,7 +19,7 @@ export default function CardDetailModalPage() {
         }
       }}
     >
-      <DialogContent className="max-h-[90vh] max-w-[760px] overflow-hidden border-border/70 bg-card p-0 shadow-[0_24px_60px_rgba(63,54,49,0.14)]">
+      <DialogContent className="max-h-[90vh] w-[94vw] max-w-[860px] overflow-hidden border-border/70 bg-card p-0 shadow-[0_24px_60px_rgba(63,54,49,0.14)]">
         <DialogTitle className="sr-only">카드 상세</DialogTitle>
         <CardDetailScene cardId={cardId} asModal />
       </DialogContent>
